5d45880e82e13dc270761f5499d901e28b71f3d4
Sourodip_18 <sourodipmahato762@gmail.com>
2026-07-10 12:37:31 +0530

    adding a new file

---
+++ script.js
@@ -0,0 +1,43 @@
+console.log("Hey this is tutorial ")
+//var is a global scope variable
+var a = 5; //this is a number data type
+var c = "Sourodip"; //this is a string data type
+var _a = "shubam";  //this is a valid name here
+// var 55a = "rohan"; this is not valid here as a variable is not started with number
+//  var b = 7;
+// console.log(a+b+10)
+// console.log(c)
+// console.log(typeof a, typeof b, typeof c)
+//  const a1 = 88;
+ //a1=a1+55;  //this is not valid as it is constant and cannot be change
+ 
+ let f = 44; //now this is a block scope variable
+//  console.log(f)
+ 
+//  console.log(a1)
+ 
+//  let b = 7;
+
+//  {
+//     var b = 770;
+//     console.log(b)
+//  }
+
+//  console.log(b)
+
+ let x = "Sourodip bhai";
+ let y = 22;
+ let z = 3.55;
+ const p = true;
+ let q = undefined;
+ let r = null; //this is a object
+
+ console.log(x, y, z, p, q, r)
+ console.log(typeof x, typeof y, typeof z, typeof p, typeof q, typeof r)
+
+ let o = {
+    "name" : "Harry",
+    "job code" : 5600,
+ }
+
+ console.log(o)
